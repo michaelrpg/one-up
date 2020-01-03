d2ea19bea7a28c27272cfc896d5dcc3b1407e6c0
Michael Gustafson <205666+michaelrpg@users.noreply.github.com>
2020-01-02 21:35:06 -0500

    Show the build path for word creation

--- main.cpp
+++ main.cpp
@@ -11,32 +11,37 @@ using namespace std;
 map<string, set<string>> wordMap;
 vector<string> currentWords;
 
-void cheatWords(int pos, bool valid, string soFar, vector<string> &results) {
+void cheatWords(int pos, bool valid, string soFar, vector<string> build, pair<vector<string>, vector<vector<string>>> &results) {
   // cout << pos << " " << soFar << endl;
   if (pos >= currentWords.size()) {
     return;
   }
 
-  cheatWords(pos + 1, valid, soFar, results);
+  cheatWords(pos + 1, valid, soFar, build, results);
 
   if (currentWords[pos].size() == 1) {
     valid = true;
   }
 
   soFar += currentWords[pos];
+  build.push_back(currentWords[pos]);
   sort(soFar.begin(), soFar.end());
   if (valid && wordMap[soFar].size() > 0) {
-    results.insert(results.end(), wordMap[soFar].begin(), wordMap[soFar].end());
+    for (auto w : wordMap[soFar]) {
+      results.first.push_back(w);
+      results.second.push_back(build);
+    }
   }
 
-  cheatWords(pos + 1, valid, soFar, results);
+  cheatWords(pos + 1, valid, soFar, build, results);
 }
 
-vector<string> cheat() {
+pair<vector<string>, vector<vector<string>>> cheat() {
   string soFar = "";
-  vector<string> results;
+  vector<string> build;
+  pair<vector<string>, vector<vector<string>>> results;
   
-  cheatWords(0, false, soFar, results);
+  cheatWords(0, false, soFar, build, results);
 
   return results;
 }
@@ -69,6 +74,8 @@ int main(int argc, char* argv[]) {
   string userInput;
 
   while (true) {
+    sort(currentWords.begin(), currentWords.end());
+
     cout << "Enter command (q=quit p=print 'a X'=add 'r X'=remove c=cheat): ";
     cin >> userInput;
 
@@ -108,10 +115,14 @@ int main(int argc, char* argv[]) {
       currentWords.erase(p);
     }
     else if (userInput[0] == 'c') {
-      vector<string> cheatWords = cheat();
+      pair<vector<string>, vector<vector<string>>> cheatWords = cheat();
 
-      for (auto w : cheatWords) {
-        cout << w << " ";
+      for (int i=0; i < cheatWords.first.size(); i++) {
+        cout << cheatWords.first[i] << ": ";
+        for (auto w : cheatWords.second[i]) {
+          cout << w << " ";      
+        }
+        cout << endl;
       }
 
       cout << endl;
